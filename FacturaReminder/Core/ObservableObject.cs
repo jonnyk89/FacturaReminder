14f5910d0d95339f59864d31c8808ba7962108c9
Ioan Krumov <ioankrumov@gmail.com>
2024-10-24 03:29:44 +0300

    Added UI but it needs functionality

---
+++ FacturaReminder/Core/ObservableObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+/*
+    INotifyPropertyChanged - mainly used for updating the UI when binding
+
+ */
+
+namespace FacturaReminder.Core
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
