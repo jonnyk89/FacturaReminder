ccb6c7cfd35ef287b85c366a0b449b43131fe174
Ioan Krumov <ioankrumov@gmail.com>
2024-10-29 09:21:51 +0200

    Search field now only appears when the Tasks tab is selected

--- FacturaReminder/MVVM/ViewModels/MainViewModel.cs
+++ FacturaReminder/MVVM/ViewModels/MainViewModel.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace FacturaReminder.MVVM.ViewModels
 {
@@ -13,7 +15,6 @@ namespace FacturaReminder.MVVM.ViewModels
         public RelayCommand TasksViewCommand { get; set; }
 
         public HomeViewModel HomeVM { get; set; }
-
         public TasksViewModel TasksVM { get; set; }
 
         private object _currentView;
@@ -28,20 +29,36 @@ namespace FacturaReminder.MVVM.ViewModels
             }
         }
 
+        private Visibility _searchVisibility;
+
+        public Visibility SearchVisibility
+        {
+            get { return _searchVisibility; }
+            set 
+            { 
+                _searchVisibility = value;
+                OnPropertyChanged();
+            }
+        }
+
+
 
         public MainViewModel()
         {
             HomeVM = new HomeViewModel();
             TasksVM = new TasksViewModel();
             CurrentView = HomeVM;
+            SearchVisibility = Visibility.Collapsed;
 
             HomeViewCommand = new RelayCommand(o =>
             {
+                SearchVisibility = Visibility.Collapsed;
                 CurrentView = HomeVM;
             });
 
             TasksViewCommand = new RelayCommand(o =>
             {
+                SearchVisibility = Visibility.Visible;
                 CurrentView = TasksVM;
             });
         }
